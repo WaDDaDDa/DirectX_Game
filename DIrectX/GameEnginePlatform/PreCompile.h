37b9db45fed6d06557d4b9d02b44489d7ec45f50
KimJunYoung <kimchk1104@naver.com>
2023-08-02 20:50:28 +0900

    김준영 : 엔진구조 변경 Camera Renderer 등등이 모두 Object을 상속받도록.  -> 다형성 증진효과

--- DIrectX/GameEnginePlatform/PreCompile.h
+++ DIrectX/GameEnginePlatform/PreCompile.h
@@ -2,7 +2,8 @@
 
 #include <GameEngineBase/PreCompile.h>
 
-
 #include <GameEngineBase/GameEngineMath.h>
 #include <GameEngineBase/GameEnginePath.h>
 #include <GameEngineBase/GameEngineDirectory.h>
+#include <GameEngineBase/GameEngineTime.h>
+#include <GameEngineBase/GameEngineString.h>
